eaecd6c2377dbcb405896917b215b0cd557128ef
siwapong kopkit <big3092541@gmal.com>
2021-06-07 13:34:22 +0700

    V1

---
+++ main.py
@@ -0,0 +1,103 @@
+import cv2
+import numpy as np
+import pytesseract
+import io
+
+
+from fastapi import FastAPI, File, UploadFile, Request
+
+from fastapi.responses import HTMLResponse
+from typing import List
+
+
+from fastapi import FastAPI, Request, File, UploadFile, BackgroundTasks
+from fastapi.templating import Jinja2Templates
+import shutil
+import ocr
+import os
+import uuid
+import json
+import base64
+
+from pydantic import BaseModel
+
+
+from typing import Optional
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+a = cv2.__version__
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World11"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Optional[str] = None):
+    return {"item_id": item_id, "q": q}
+
+
+# upload single file
+@app.post("/img")
+async def up_img_book(file: UploadFile = File(...)):
+    contents = await file.read()
+    image_stream = io.BytesIO(contents)
+    image_stream.seek(0)
+    file_bytes = np.asarray(bytearray(image_stream.read()), dtype=np.uint8)
+    frame = cv2.imdecode(file_bytes, cv2.IMREAD_COLOR)
+    label =  ocr.read_img(frame)
+
+    return  { "File Name": file.filename, "label": label}
+
+
+##---------------------
+class Abc(BaseModel):
+    image : str 
+
+
+# upload single file
+@app.post("/imgg")
+async def up_img_book(file: Abc ):
+    contents = base64.b64decode(file.image)
+    image_stream = io.BytesIO(contents)
+    image_stream.seek(0)
+    file_bytes = np.asarray(bytearray(image_stream.read()), dtype=np.uint8)
+    frame = cv2.imdecode(file_bytes, cv2.IMREAD_COLOR)
+    label =  ocr.read_img(frame)
+    return  { "label": label}
+
+
+
+##-----------------
+
+
+#app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/0")
+def home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+
+
+
+@app.post("/extract_text") 
+async def extract_text(request: Request):
+    label = ""
+    if request.method == "POST":
+        form = await request.form()
+        # file = form["upload_file"].file
+        contents = await form["upload_file"].read()
+        image_stream = io.BytesIO(contents)
+        image_stream.seek(0)
+        file_bytes = np.asarray(bytearray(image_stream.read()), dtype=np.uint8)
+        frame = cv2.imdecode(file_bytes, cv2.IMREAD_COLOR)
+        label =  ocr.read_img(frame)
+       
+        # return {"label": label}
+   
+    return templates.TemplateResponse("index.html", {"request": request, "label": label})
---
+++ ocr.py
@@ -0,0 +1,180 @@
+import cv2
+import numpy as np
+import pytesseract
+import io
+
+
+
+def read_img(img):
+
+    width=700
+    height=700
+    y1=0
+    y2=700
+    x1=500
+    x2=700
+    #img=cv2.imread('IMG_25640601_111717.jpg') #read image
+
+    img=cv2.resize(img,(width,height)) #resize image
+
+    imgSAVE = img
+
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) #convert roi into gray
+    Blur=cv2.GaussianBlur(gray,(5,5),1) #apply blur to roi
+    Canny=cv2.Canny(Blur,10,50) #apply canny to roi
+
+    #Find my contours
+    contours =cv2.findContours(Canny,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)[0]
+
+    #Loop through my contours to find rectangles and put them in a list, so i can view them individually later.
+    cntrRect = []
+    for i in contours:
+            epsilon = 0.05*cv2.arcLength(i,True)
+            approx = cv2.approxPolyDP(i,epsilon,True)
+            if len(approx) == 4:
+                #cv2.drawContours(img,cntrRect,-1,(0,255,0),2)
+                #cv2.imshow('img Rect ONLY',img)
+                cntrRect.append(approx)
+
+                if len(contours) != 0:
+                    # draw in blue the contours that were founded
+    #                 cv2.drawContours(output, contours, -1, 255, 3)
+
+                    # find the biggest countour (c) by the area
+                    c = max(contours, key = cv2.contourArea)
+                    x,y,w,h = cv2.boundingRect(c)
+
+                    # draw the biggest contour (c) in green
+    #                 cv2.rectangle(output,(x,y),(x+w,y+h),(0,255,0),2)
+
+    img0 = cv2.rectangle(img,(x,y),(w+x,h+y),(0,0,255),10)
+
+    #cv2.imshow('img 000',img0)
+
+
+    cropimg = img[y:y+h, x:x+w]
+
+    #resized image
+    width = 410
+    height = 310
+    dim = (width, height)
+     
+    # resize image
+    resizedcropimg = cv2.resize(cropimg, dim, interpolation = cv2.INTER_AREA)
+
+
+    #converting image into gray scale image
+
+    resized = cv2.cvtColor(resizedcropimg, cv2.COLOR_BGR2GRAY)
+
+    # converting it to binary image by Thresholding
+
+    # this step is require if you have colored image because if you skip this part
+
+    # then tesseract won't able to detect text correctly and this will give incorrect result
+
+    resized = cv2.threshold(resized, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+
+
+    #img99=cv2.imread('IMG_25640601_111717.jpg')
+    text = pytesseract.image_to_string(resized, lang='tha+eng')
+
+
+    #crop image 01 1 9209 00014 28 8 ID card number
+    y= int(10.32*height*0.01)
+    x=int(43.9*width*0.01)
+    h=28
+    w=150
+    crop01 = resized[y:y+h, x:x+w]
+    resultIDcardnumber = pytesseract.image_to_string(crop01, lang='tha+eng')
+    resultIDcardnumber = resultIDcardnumber.replace("\n", "")
+    resultIDcardnumber = resultIDcardnumber.replace("\f", "")
+
+    #crop image 02 นาย ศิรพงษ์ กอบกิจ resultnamethai
+    y=int(18.387*height*0.01)
+    x=int(28.5365*width*0.01)
+    h=40
+    w=300
+    crop01 = resized[y:y+h, x:x+w]
+    resultnamethai = pytesseract.image_to_string(crop01, lang='tha+eng')
+    resultnamethai = resultnamethai.replace("\n", "")
+    resultnamethai = resultnamethai.replace("\f", "")
+
+    #crop image 03 Mr. Siwapong resultnameeng
+    y=int(28.387*height*0.01)
+    x=int(39.02*width*0.01)
+    h=30
+    w=180
+    crop01 = resized[y:y+h, x:x+w]
+    resultnameeng = pytesseract.image_to_string(crop01, lang='eng')
+    resultnameeng = resultnameeng.replace("\n", "")
+    resultnameeng = resultnameeng.replace("\f", "")
+
+    #crop image 04 Kopkit resultlastname
+    y=int(35.483*height*0.01)
+    x=int(43.9*width*0.01)
+    h=30
+    w=180
+    crop01 = resized[y:y+h, x:x+w]
+    resultlastname = pytesseract.image_to_string(crop01, lang='eng')
+    resultlastname = resultlastname.replace("\n", "")
+    resultlastname = resultlastname.replace("\f", "")
+
+    #crop image 05 30 กย, 2541 resultbirthday
+    y=int(43.548*height*0.01)
+    x=int(43.17*width*0.01)
+    h=25
+    w=180
+    crop01 = resized[y:y+h, x:x+w]
+    resultbirthday = pytesseract.image_to_string(crop01, lang='tha')
+    resultbirthday = resultbirthday.replace("\n", "")
+    resultbirthday = resultbirthday.replace("\f", "")
+
+    #crop image 06 30 Sep. 1998 resultbirthdayeng
+    y=int(50*height*0.01)
+    x=int(50*width*0.01)
+    h=27
+    w=120
+    crop01 = resized[y:y+h, x:x+w]
+    resultbirthdayeng = pytesseract.image_to_string(crop01, lang='eng')
+    resultbirthdayeng = resultbirthdayeng.replace("\n", "")
+    resultbirthdayeng = resultbirthdayeng.replace("\f", "")
+
+    #crop image 07 ศาสนา พุทธ
+    y=int(58.06*height*0.01)
+    x=int(30*width*0.01)
+    h=25
+    w=180
+    crop01 = resizedcropimg[y:y+h, x:x+w]
+    resultreligion = pytesseract.image_to_string(crop01, lang='tha')
+    resultreligion = resultreligion.replace("\n", "")
+    resultreligion = resultreligion.replace("\f", "")
+
+    #crop image 08 246 หมู่ที 1 ต.เขาไพร resultaddress
+    y=int(64.516*height*0.01) #200
+    x=int(20.5*height*0.01) #65
+    h=28
+    w=180
+    crop01 = resizedcropimg[y:y+h, x:x+w]
+    resultaddress = pytesseract.image_to_string(crop01, lang='tha')
+    resultaddress = resultaddress.replace("\n", "")
+    resultaddress = resultaddress.replace("\f", "")
+
+    #crop image 09  resultresultlastaddress
+    y=int(70.716*height*0.01) #200
+    x=int(10.5*height*0.01) #65
+    h=28
+    w=180
+    crop01 = resizedcropimg[y:y+h, x:x+w]
+    resultresultlastaddress = pytesseract.image_to_string(crop01, lang='tha+eng')
+    resultresultlastaddress = resultresultlastaddress.replace("\n", "")
+    resultresultlastaddress = resultresultlastaddress.replace("\f", "")
+
+    return {#"image_to_string":text,
+     "resultIDcardnumber":resultIDcardnumber, "resultnamethai":resultnamethai, "resultnameeng":resultnameeng,
+     "resultlastname":resultlastname, "resultbirthday":resultbirthday, "resultbirthdayeng":resultbirthdayeng
+     , "resultreligion":resultreligion, "resultaddress":resultaddress+resultresultlastaddress}
+
+#  , file: bytes = File(...)
+
+
